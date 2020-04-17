4357a07390eeec1ef1c10bee2d0878327097ab33
Sharon <sharon.brakha@gmail.com>
2020-04-17 17:04:26 +0300

    Week 2 Day 1

---
+++ Week_2/day_1/Exercises_XP_Gold.js
@@ -0,0 +1,13 @@
+
+// Exercise 1 
+
+var me = ["my","favorite","color","is","blue"]
+var sentence = me.join(" ")
+
+
+// Exercise 3
+
+
+var first = Number(prompt('Your first number'));
+var second = Number(prompt('Your second number'));
+alert(first+second);
