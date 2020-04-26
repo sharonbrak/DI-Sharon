62e9591b4f1a06049f0260be5799d3765ef8254d
Sharon <sharon.brakha@gmail.com>
2020-04-26 09:20:59 +0300

    Improved Calculator / Adding Display

--- Week_3/day_5/calculatorsb.js
+++ Week_3/day_5/calculatorsb.js
@@ -1,56 +1,50 @@
 
 var operator = ['+', '-', '*', '/'];
-var number1 = 0
-var number2 = 0
-var op = ''
-var numberfull = []
+var number1 = 0;
+var number2 = 0;
+var op = '';
+var numberfull = [];
+var display = document.getElementById("display");
 
+
+function calcul(x,y) {
+    if (op ==='+') {
+        return x+y;
+    } else if (op ==='-') {
+        return x-y;
+    } else if (op ==='*') {
+        return x*y;
+    } else if (op ==='/') {
+        return x/y;
+    }
+}
 
 function my_f(choice) {
     if (typeof choice === 'number') {
         if (op === '') {
             numberfull.push (choice);
             number1 = Number(numberfull.join(""));
+            document.getElementById("display").value = number1;
         } else {
             numberfull.push (choice);
             number2 = Number(numberfull.join(""));
+            document.getElementById("display").value = number2;
         }
     }
     
     else if (operator.indexOf(choice) >=0) {
        op = choice;
        numberfull = [];
+       document.getElementById("display").value = op;
     } 
 
     else if (choice === '=') {
-        if (op ==='+') {
-            alert(number1 + number2);
-            number1 = 0;
-            number2 = 0;
-            op = '';
-            numberfull = [];
-        } else if (op ==='-') {
-            alert(number1 - number2);
-            number1 = 0;
-            number2 = 0;
-            op = '';
-            numberfull = [];
-        } else if (op ==='*') {
-            alert(number1 * number2);
-            number1 = 0;
-            number2 = 0;
-            op = '';
-            numberfull = [];
-        } else if (op ==='/') {
-            alert(number1 / number2);
-            number1 = 0;
-            number2 = 0;
-            op = '';
-            numberfull = [];
-        }
-    
+        display.value = calcul(number1,number2);
+        number1 = 0;
+        number2 = 0;
+        op = '';
+        numberfull = [];
     }
-
 }
 
 function clearcal() {
@@ -58,6 +52,9 @@ function clearcal() {
     number2 = 0
     op = ''
     numberfull = []
-    alert('Your calculator is now clean!')
+    document.getElementById("display").value = 0;
 }
+
+
+
 
